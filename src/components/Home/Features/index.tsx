adb50ba33f0eb2744728380595957e05da7b197f
julieg18 <juliannagal18@gmail.com>
2021-12-03 14:26:51 -0600

    Refactor how Features creates li elements

--- src/components/Home/Features/index.tsx
+++ src/components/Home/Features/index.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import * as styles from './index.module.css'
 
-const featuresData: Array<string> = [
-  '<strong>Unambiguous link</strong> between data, code, model and metrics',
-  '<strong>Standardised model packaging</strong> with environment and input data specification',
-  'Model lifecycle management using <strong>GitOps approach</strong>',
-  '<strong>Cloud-agnostic</strong> model deployment'
+const featuresData: Array<JSX.Element> = [
+  <>
+    <strong>Unambiguous link</strong> between data, code, model and metrics
+  </>,
+  <>
+    <strong>Standardised model packaging</strong> with environment and input
+    data specification
+  </>,
+  <>
+    Model lifecycle management using <strong>GitOps approach</strong>
+  </>,
+  <>
+    <strong>Cloud-agnostic</strong> model deployment
+  </>
 ]
 
 const Features: React.FC = () => {
@@ -15,12 +24,10 @@ const Features: React.FC = () => {
         Why MLEM<span>_</span>
       </h2>
       <ul className={styles.features__list}>
-        {featuresData.map((string, i) => (
-          <li
-            className={styles.features__feature}
-            key={i}
-            dangerouslySetInnerHTML={{ __html: string }}
-          />
+        {featuresData.map((el, i) => (
+          <li className={styles.features__feature} key={i}>
+            {el}
+          </li>
         ))}
       </ul>
     </section>
