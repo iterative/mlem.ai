a0f9c8e39d1b4525a38faaea0389a5c554825801
julieg18 <juliannagal18@gmail.com>
2021-12-07 14:17:17 -0600

    Place tbd comment in home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import Layout from '../components/Layout'
 import Home from '../components/Home'
-
+// tbd comment
 const IndexPage: React.FC = () => (
   <Layout title="Home">
     <Home />
