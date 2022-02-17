c11957ffbcb42515c55e8b521bf09aed01182cee
Julie <43496356+julieg18@users.noreply.github.com>
2022-02-17 15:03:24 -0600

    Fix typos in 404 page (#37)

--- src/components/NotFound/index.tsx
+++ src/components/NotFound/index.tsx
@@ -10,7 +10,7 @@ const NotFoundPage: React.FC = () => (
     <header className={styles.header}>
       <h1 className={styles.header__title}>Page not found</h1>
       <p className={styles.header__text}>
-        The page you&apos;re looking doesn&apos;t exist or another error
+        The page you&apos;re looking for doesn&apos;t exist, or an error
         occurred. Please check your internet connection or{' '}
         <SmartLink href="/">go back to home</SmartLink>.
       </p>
