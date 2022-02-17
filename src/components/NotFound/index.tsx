6e29471104c4c95e38c9126f6ca9af87b286894f
Julie <43496356+julieg18@users.noreply.github.com>
2022-02-17 06:31:02 -0600

    Improve NotFound page (#36)
    * take off test error
    * fix typo
    * center page content and add font sizes/styles

--- src/components/NotFound/index.tsx
+++ src/components/NotFound/index.tsx
@@ -1,28 +1,22 @@
-import React, { useEffect } from 'react'
+import * as React from 'react'
 import SmartLink from '../SmartLink'
 import NavBar from '../NavBar'
 import Footer from '../Footer'
 import * as styles from './index.module.css'
 
-const NotFoundPage: React.FC = () => {
-  useEffect(() => {
-    throw Error('Fake error for testing Sentry')
-  }, [])
-
-  return (
-    <>
-      <NavBar />
-      <header className={styles.header}>
-        <h1>Page not found</h1>
-        <p className={styles.header__text}>
-          The page you&apos;re looking doesn&apos;t exist or another error
-          occured. Please check your internet connection or{' '}
-          <SmartLink href="/">go back to home</SmartLink>.
-        </p>
-      </header>
-      <Footer />
-    </>
-  )
-}
+const NotFoundPage: React.FC = () => (
+  <>
+    <NavBar />
+    <header className={styles.header}>
+      <h1 className={styles.header__title}>Page not found</h1>
+      <p className={styles.header__text}>
+        The page you&apos;re looking doesn&apos;t exist or another error
+        occurred. Please check your internet connection or{' '}
+        <SmartLink href="/">go back to home</SmartLink>.
+      </p>
+    </header>
+    <Footer />
+  </>
+)
 
 export default NotFoundPage
