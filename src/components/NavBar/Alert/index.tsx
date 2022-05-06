a999e2c56f2621eccf6c448ffb254a44922a82a7
julieg18 <juliannagal18@gmail.com>
2022-05-06 08:01:24 -0500

    Fix bugs

--- src/components/NavBar/Alert/index.tsx
+++ src/components/NavBar/Alert/index.tsx
@@ -6,10 +6,12 @@ import * as styles from './index.module.css'
 
 const Alert: React.FC<{ collapsed: boolean }> | false = ({ collapsed }) => (
   <div className={cn(styles.alert, collapsed && styles.collapsed)}>
-    Learn how to build{' '}
-    <SmartLink href="https://iterative.ai/not-existent-page">
-      Model Registry with MLEM
-    </SmartLink>
+    <p>
+      Learn how to build{' '}
+      <SmartLink href="https://iterative.ai/not-existent-page">
+        Model Registry with MLEM
+      </SmartLink>
+    </p>
   </div>
 )
 
