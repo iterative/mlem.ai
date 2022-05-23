f30d83862f714a105c8972773804db77b9e8c423
rogermparent <rogermparent@gmail.com>
2022-05-23 19:28:41 -0400

    Add heights at all media queries and remove inner wrapper

--- src/components/NavBar/Alert/index.tsx
+++ src/components/NavBar/Alert/index.tsx
@@ -6,12 +6,10 @@ import * as styles from './index.module.css'
 
 const Alert: React.FC<{ collapsed: boolean }> = ({ collapsed }) => (
   <div className={cn(styles.alert, collapsed && styles.collapsed)}>
-    <p>
-      Learn how to build{' '}
-      <SmartLink href="https://iterative.ai/model-registry">
-        Model Registry with MLEM
-      </SmartLink>
-    </p>
+    Learn how to build{' '}
+    <SmartLink href="https://iterative.ai/model-registry">
+      Model Registry with MLEM
+    </SmartLink>
   </div>
 )
 
