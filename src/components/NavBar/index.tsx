69aeaef41b25ae3556dae731dc1912f432572ff5
Roger <rogermparent@gmail.com>
2022-05-31 09:56:51 -0400

    Fix alert box (#64)
    * Expand header width to account for alert
    
    * Add scroll-based alert collapsing
    
    * Add heights at all media queries and remove inner wrapper
    
    * fix scroll issue
    
    Co-authored-by: Thomas Kunwar <yathomasi@gmail.com>

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import cn from 'classnames'
 import SmartLink from '../SmartLink'
 import Button from '../Button'
 import SocialIcon, { ISocialIconProps } from './SocialIcon'
@@ -6,6 +7,7 @@ import mlemLogo from '../../images/mlem-logo.png'
 import OtherToolsPopup from './OtherToolsPopup'
 import Alert from './Alert'
 import * as styles from './index.module.css'
+import { useHeaderIsScrolled } from '@dvcorg/gatsby-theme-iterative/src/utils/front/scroll'
 
 const socialLinks: Array<ISocialIconProps> = [
   {
@@ -22,51 +24,62 @@ const socialLinks: Array<ISocialIconProps> = [
 ]
 
 const NavBar: React.FC = () => {
+  const scrolled = useHeaderIsScrolled()
+
   return (
-    <header className={styles.header}>
-      <Alert collapsed={false} />
-      <nav className={styles.nav}>
-        <SmartLink href="/" className={styles.nav__logoLink}>
-          <img
-            alt="Go to home page"
-            src={mlemLogo}
-            className={styles.nav__brandLogo}
-            width={108}
-            height={24}
-          />
-        </SmartLink>
-        <SmartLink
-          className={styles.nav__brandCompany}
-          href="https://iterative.ai/"
-        >
-          by <span>iterative.ai</span>
-        </SmartLink>
-        <SmartLink href="/doc" className={styles.nav__link}>
-          Docs
-        </SmartLink>
-        <SmartLink
-          href="https://learn.iterative.ai/"
-          className={styles.nav__link}
-        >
-          Course
-        </SmartLink>
-        <OtherToolsPopup navItemClassName={styles.nav__link} />
-        <Button className={styles.nav__button} icon="github" disabled>
-          Coming Soon
-        </Button>
-        <ul className={styles.nav__icons}>
-          {socialLinks.map(({ icon, href, label, target }, i) => (
-            <li key={i}>
-              <SocialIcon
-                target={target}
-                icon={icon}
-                href={href}
-                label={label}
-              />
-            </li>
-          ))}
-        </ul>
-      </nav>
+    <header>
+      <div
+        className={cn(
+          styles.placeholder,
+          scrolled && styles.collapsed,
+          styles.withAlert
+        )}
+      />
+      <div className={cn(styles.header, scrolled || styles.hasAlert)}>
+        <Alert collapsed={scrolled} />
+        <nav className={styles.nav}>
+          <SmartLink href="/" className={styles.nav__logoLink}>
+            <img
+              alt="Go to home page"
+              src={mlemLogo}
+              className={styles.nav__brandLogo}
+              width={108}
+              height={24}
+            />
+          </SmartLink>
+          <SmartLink
+            className={styles.nav__brandCompany}
+            href="https://iterative.ai/"
+          >
+            by <span>iterative.ai</span>
+          </SmartLink>
+          <SmartLink href="/doc" className={styles.nav__link}>
+            Docs
+          </SmartLink>
+          <SmartLink
+            href="https://learn.iterative.ai/"
+            className={styles.nav__link}
+          >
+            Course
+          </SmartLink>
+          <OtherToolsPopup navItemClassName={styles.nav__link} />
+          <Button className={styles.nav__button} icon="github" disabled>
+            Coming Soon
+          </Button>
+          <ul className={styles.nav__icons}>
+            {socialLinks.map(({ icon, href, label, target }, i) => (
+              <li key={i}>
+                <SocialIcon
+                  target={target}
+                  icon={icon}
+                  href={href}
+                  label={label}
+                />
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
     </header>
   )
 }
