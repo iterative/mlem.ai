0bb8ae9107347709e7c851a4529a10378e098523
rogermparent <rogermparent@gmail.com>
2022-12-07 15:31:35 -0500

    Remove broken sentry config

--- sentry.config.js
+++ sentry.config.js
@@ -2,4 +2,3 @@ import sentryConfig from '@dvcorg/gatsby-theme-iterative/sentry-config'
 import * as Sentry from '@sentry/gatsby'
 console.log(sentryConfig)
 Sentry.init(sentryConfig)
-window.Sentry = Sentry
