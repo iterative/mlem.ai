1a93713604bf0fd7ac30626fdcb171bf826b80dc
rogermparent <rogermparent@gmail.com>
2022-12-07 15:31:03 -0500

    Add sentry test code to homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,22 @@
 import * as React from 'react'
 import Layout from '../components/Layout'
 import Home from '../components/Home'
+import * as Sentry from '@sentry/gatsby'
 
-const IndexPage: React.FC = () => (
-  <Layout title="Home">
-    <Home />
-  </Layout>
-)
+const IndexPage: React.FC = () => {
+  Sentry.captureMessage('Test message!')
+  return (
+    <Layout title="Home">
+      <button
+        onClick={() => {
+          throw new Error('Test thrown error!')
+        }}
+      >
+        Throw an error
+      </button>
+      <Home />
+    </Layout>
+  )
+}
 
 export default IndexPage
