a33cab8aae285a39137ec93c3378bc2e20ab13b2
rogermparent <rogermparent@gmail.com>
2022-12-07 15:10:40 -0500

    Add sentry test lines

--- sentry.config.js
+++ sentry.config.js
@@ -1,4 +1,5 @@
 import sentryConfig from '@dvcorg/gatsby-theme-iterative/sentry-config'
 import * as Sentry from '@sentry/gatsby'
-
+console.log(sentryConfig)
 Sentry.init(sentryConfig)
+window.Sentry = Sentry
