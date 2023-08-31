c802a6d2ea414578c13875ca0d9465131274c029
Alexander Guschin <1aguschin@gmail.com>
2023-08-31 12:00:40 +0200

    add archive message

--- src/@dvcorg/gatsby-theme-iterative/components/LayoutHeader/Alert/index.tsx
+++ src/@dvcorg/gatsby-theme-iterative/components/LayoutHeader/Alert/index.tsx
@@ -1,6 +1,5 @@
 import cn from 'classnames'
 import React from 'react'
-import SmartLink from '../../../../../components/SmartLink'
 
 import * as styles from './index.module.css'
 
@@ -8,14 +7,11 @@ const Alert: React.FC<{ collapsed: boolean }> = ({ collapsed }) => (
   <div className={cn(styles.alert, collapsed && styles.collapsed)}>
     <div className={styles.text}>
       <span className={styles.icon} role="img" aria-label="rocket">
-        🚀
+        📦
       </span>{' '}
       <p>
-        MLEM now offers{' '}
-        <SmartLink href="https://iterative.ai/blog/mlem-k8s-sagemaker">
-          deployment to Kubernetes and Sagemaker
-        </SmartLink>{' '}
-        with a single command.
+        MLEM project is archived. You can use it, but no further support is
+        planned. Thank you for being with us!
       </p>
     </div>
   </div>
